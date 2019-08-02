846f7a860cb62bc7fa382f52b8c69e62545c7ed2
Dukyoon <miso1061402ggm@gmail.com>
2019-08-03 04:34:35 +0900

    switch, innerHTML, 숫자인지 확인

--- jsFile.js
+++ jsFile.js
@@ -3,7 +3,7 @@ var output = document.getElementById("outPut");
 function getRadioSelection(name) {
     var el = document.getElementsByName(name);
 
-    for (i = 0; i < el.length; i++) {
+    for (let i = 0; i < el.length; i++) {
         if (el[i].checked === true) {
             return el[i].value;
         }
@@ -12,19 +12,30 @@ function getRadioSelection(name) {
 }
 
 function calc() {
-    alert(getRadioSelection("action"));
-
     var a = document.getElementById("a").value;
     var b = document.getElementById("b").value;
+    var output = document.getElementById("outPut");
 
-    if (getRadioSelection("action") === "add") {
-        alert((parseInt(a, 2) + parseInt(b, 2)).toString(2));
-    } else if (getRadioSelection("action") === "subtract") {
-        alert((parseInt(a, 2) - parseInt(b, 2)).toString(2));
-    } else if (getRadioSelection("action") === "divide") {
-        alert((parseInt(a, 2) / parseInt(b, 2)).toString(2));
-    } else if (getRadioSelection("action") === "multiply") {
-        alert((parseInt(a, 2) * parseInt(b, 2)).toString(2));
+    if (isNaN(a) === false && isNaN(b) === false) {
+        switch(getRadioSelection("action")) {
+            case "add" :
+                output.innerHTML = ((parseInt(a, 2) + parseInt(b, 2)).toString(2));
+                break;
+                
+            case "subtract" :
+                output.innerHTML = ((parseInt(a, 2) - parseInt(b, 2)).toString(2));
+                break;
+                
+            case "divide" :
+                output.innerHTML = ((parseInt(a, 2) / parseInt(b, 2)).toString(2));
+                break;
+                
+            case "multiply" :
+                output.innerHTML = ((parseInt(a, 2) * parseInt(b, 2)).toString(2));
+                break;
+        }
+    } else {
+        alert("숫자가 아닙니다")
     }
 
 }
